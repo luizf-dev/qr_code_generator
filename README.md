5212d1f6aa9ff90eb3a027abbf47404d783f3171
Luiz Fernando <luizfvidal3@hotmail.com>
2023-12-28 14:33:03 -0300

    add transitions no css

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ Aplicação para gerar QR Code e realizar o download do mesmo em formato PNG.
 * HTML
 * CSS
 * Javascript
+
+
+# Fluxo do javascript
+
+* Seleção de Elementos HTML:
+    image, input, generate, e download são variáveis que armazenam referências a elementos HTML com os IDs correspondentes.
+
+* Definição da URL da API:
+    api e api2 formam a URL base da API que será usada para gerar QR codes. api2 contém parâmetros para o tamanho da imagem e os dados do QR code.
+
+* Função addClass:
+    Uma função que adiciona a classe 'loaded' à imagem.
+
+    Evento de Clique no Botão "Generate":
+        Quando o botão "Generate" é clicado, verifica se o campo de entrada não está vazio.
+        Se não estiver vazio, a função addClass é chamada após um atraso de 300 milissegundos, e a classe 'loaded' é adicionada à imagem.
+        A imagem SRC é atualizada para a URL do QR code gerado pela API, com base nos dados inseridos no campo de entrada.
+        O valor do campo de entrada é limpo.
+
+    Evento de Clique no Botão "Download":
+        Quando o botão "Download" é clicado, uma requisição assíncrona é feita para obter a imagem do QR code.
+        A imagem é convertida em um blob.
+        Um elemento de link temporário é criado, configurado com o blob e um nome de arquivo para realizar o download.
+        O link é clicado, iniciando o processo de download.
--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-//* Arquivo javascript */ 
+//* Seleciona os elementos do HTML pelos IDs
 const image = document.getElementById('img');
 const input = document.getElementById('input');
 const generate = document.getElementById('button-generate');
 const download = document.getElementById('button-download');
 
-
+//* Define a URL da API para gerar QR codes
 const api = 'https://api.qrserver.com/v1/';
 const api2 = 'create-qr-code/?size=200x200&data=';
 
+
+//* Função para adicionar a classe 'loaded' à imagem
 const addClass = () =>{
-    image.classList.add('qr');
+    
+    image.classList.add('loaded');
 }
 
+//* Adiciona um evento ao botão de geração de QR code
 generate.addEventListener("click", () => {
 
     if(input.value == ""){
         alert("Insira informações no campo!")
     }else{
+        //* Adiciona a classe 'loaded' após um atraso de 300 milissegundos
         setTimeout(addClass, 300);
     }    
 
+    //* Atualiza a imagem SRC para a URL do QR code gerado pela API
     image.src = `${api}${api2}${input.value}`;    
+    //*limpa o valor do campo de entrada
     input.value = "";
 }); 
 
+//* Adiciona um  evento ao botão de download
 download.addEventListener("click",  async () => {   
 
+    //* Faz uma requisição assíncrona para obter a imagem
     const response = await fetch(image.src);
+
+    //*A constante blob  refere-se a um objeto Blob (Binary Large Object) em JavaScript.
+    //*a constante blob é criada para armazenar o conteúdo binário da imagem obtida pela requisição assíncrona (fetch). 
     const blob = await response.blob();
+
+
+    //* Cria um elemento de link temporário para realizar o download
     const downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(blob);
     downloadLink.download = "qrcode.png";
